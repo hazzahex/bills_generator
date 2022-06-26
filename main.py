12070f3419a7f0576957a3a31fa2d6c97b8cd65c
Harry Hextall <Harry.Hextall@dyson.com>
2022-06-26 14:59:57 +0100

    Additional updates. Per housemate weighting. Printing looks different and more descriptive.

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 import pandas as pd
 
 keywords = ["MILK AND", "BT CONSUMER", "BRISTOL CC L TAX", "BRITISH GAS", "TV LICENCE", "WESSEXWATER", "MM RECUR"]
-housemates = ["Elise", "Annabel", "Lizzy", "Harry"]
+housemates = ["Annabel", "Lizzy", "Harry", "Arty", "Rory", "Elise"]
 
-df = pd.read_csv('data/jan2022.csv')
-df = df.drop("Number", axis=1)
-df = df.drop("Account", axis=1)
-df = df.drop("Subcategory", axis=1)
+portions = []
+
+
+def horizontal_rule():
+    print()
+    print('----------------------------------------')
+    print()
+
+
+for housemate in housemates:
+    portions.append(int(input("How many days has " + housemate + " been in the house this month? ")))
+
+total_housemate_days = sum(portions)
+horizontal_rule()
+print(f"Dividing bills by {total_housemate_days} days...")
+
+df = pd.read_csv('data/may2022.csv').drop(["Number", "Account", "Subcategory"], axis=1)
 
 df = df[df.Memo.str.contains('|'.join(keywords))]
-df["Each"] = df["Amount"] / len(housemates)
-
+df["Micro"] = df["Amount"] / total_housemate_days
+total_micro = 0 - df["Micro"].sum()
+horizontal_rule()
+print(f"Bills table:")
 print(df)
-print(f"Total Bills = {0 - df['Amount'].sum()}")
-print(f"Each = {0 - df['Each'].sum()}")
-
-
+horizontal_rule()
+print(f"Housemate breakdown...")
+for index, housemate in enumerate(housemates):
+    total = total_micro * portions[index]
+    amount_string = '£{:.2f}'.format(total_micro * portions[index])
+    print(f"{housemate} - {amount_string}")
