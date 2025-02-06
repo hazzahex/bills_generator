4c46c6d16fc469c878cd7ef9bbbaf7ffafc1779f
Harry Hextall <hazzahex@gmail.com>
2025-02-06 17:51:38 +0100

    Various changes

--- main.py
+++ main.py
@@ -1,38 +1,57 @@
 import pandas as pd
 
+# Keywords to identify bills
 keywords = ["MILK AND", "BT CONSUMER", "BRISTOL CC L TAX",
-            "BRITISH GAS", "TV LICENCE", "WESSEXWATER", "MM RECUR"]
-housemates = ["Annabel", "Harry", "Arty", "Bex", "Elise"]
+            "BRITISH GAS", "TV LICENCE", "WESSEXWATER", "MM RECUR", "BT GROUP PLC"]
 
-portions = []
+# Housemates list
+housemates = ["Annabel", "Arty", "Pippa", "Alex"]
 
 
 def horizontal_rule():
-    print()
-    print('----------------------------------------')
-    print()
+    print("----------------------------------------")
 
 
-for housemate in housemates:
-    portions.append(int(input("How many days has " +
-                    housemate + " been in the house this month? ")))
+def main():
+    # Collect days each housemate has been in the house this month
+    portions = []
+    for housemate in housemates:
+        portions.append(
+            int(input(f"How many days has {housemate} been in the house this month? ")))
 
-total_housemate_days = sum(portions)
-horizontal_rule()
-print(f"Dividing bills by {total_housemate_days} days...")
+    total_housemate_days = sum(portions)
+    horizontal_rule()
+    print(f"Dividing bills by {total_housemate_days} days...")
 
-df = pd.read_csv('data/jan2023.csv',
-                 encoding="ISO-8859-1").drop(["Number", "Account", "Subcategory"], axis=1)
+    # Read bills data from CSV
+    df = pd.read_csv('data/jan2025.csv', encoding="ISO-8859-1")
 
-df = df[df.Memo.str.contains('|'.join(keywords))]
-df["Micro"] = df["Amount"] / total_housemate_days
-total_micro = 0 - df["Micro"].sum()
-horizontal_rule()
-print(f"Bills table:")
-print(df)
-horizontal_rule()
-print(f"Housemate breakdown...")
-for index, housemate in enumerate(housemates):
-    total = total_micro * portions[index]
-    amount_string = '£{:.2f}'.format(total)
-    print(f"{housemate} - {amount_string}")
+    # Filter bills based on keywords
+    df = df[df['Memo'].str.contains('|'.join(keywords))]
+
+    # Remove specific bill entry
+    df = df[~df['Memo'].str.contains("BRITISH GAS SERVIC")]
+
+    # Calculate micro portion for each bill
+    df["Micro"] = df["Amount"] / total_housemate_days
+
+    total_micro = -df["Micro"].sum()
+    horizontal_rule()
+    print("Bills table:")
+
+    thin_table = df.drop(["Number", "Account", "Subcategory"], axis=1)
+    thin_table['Memo'] = thin_table['Memo'].str.strip().str.split(r'\\|\t').str[0].str.strip()
+
+    print(thin_table)
+    horizontal_rule()
+    print("Housemate breakdown...")
+
+    # Distribute bills among housemates
+    for housemate, portion in zip(housemates, portions):
+        total = total_micro * portion
+        amount_string = '£{:.2f}'.format(total)
+        print(f"{housemate} - {amount_string}")
+
+
+if __name__ == "__main__":
+    main()
